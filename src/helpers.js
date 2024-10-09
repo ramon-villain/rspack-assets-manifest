ac2b4162e409abbfd4ec78f8807efcbc559e5637
Ramon Villain <ramon.villain@gmail.com>
2024-10-08 17:40:29 -0700

    Get Buffer/string from source.Source

--- src/helpers.js
+++ src/helpers.js
@@ -70,14 +70,14 @@ function filterHashes(hashes) {
  * See {@link https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity|Subresource Integrity} at MDN
  *
  * @param  {string[]} hashes - The algorithms you want to use when hashing `content`
- * @param  {string} content - File contents you want to hash
+ * @param  {Buffer | string } content - File contents you want to hash
  * @return {string} SRI hash
  */
 function getSRIHash(hashes, content) {
   return Array.isArray(hashes)
     ? hashes
         .map(hash => {
-          const integrity = crypto.createHash(hash).update(content, 'utf8').digest('base64');
+          const integrity = crypto.createHash(hash).update(content).digest('base64');
 
           return `${hash}-${integrity}`;
         })
